4485906131ed5585954d5e0063600e4e0ad1d101
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-10 21:47:21 +0300

    Changed Code approach to use asnc/await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,45 +2,47 @@
 
 const request = require('request');
 
-// Check if a number argument was provided
-if (process.argv.length !== 3 || isNaN(Number(process.argv[2]))) {
-  console.error('Usage: ./js_file.js <film_number>');
-  process.exit(1); // Exit with a non-zero status code to indicate an error
+function makeRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
 }
 
-const filmNumber = process.argv[2]; // Get the film number from command-line argument
+async function getCharacters (movieid) {
+  const apiURL = `https://swapi-api.alx-tools.com/api/films/${movieid}`;
 
-// Construct the API endpoint with the film number
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${filmNumber}`;
+  try {
+    const filmResponse = await makeRequest(apiURL);
+    const filmData = JSON.parse(filmResponse);
+    const characters = filmData.characters;
 
-// Make a request to the constructed endpoint
-request(apiUrl, function (error, response, body) {
-  // Check for errors and HTTP status code
-  if (!error && response.statusCode === 200) {
-    // Parse the JSON response
-    const filmsJson = JSON.parse(body);
-
-    // Get the list of characters URLs
-    const charactersUrls = filmsJson.characters;
-
-    // Iterate through the character URLs
-    charactersUrls.forEach(function (characterUrl) {
-      // Make a request to each character's URL
-      request(characterUrl, function (characterError, characterResponse, characterBody) {
-        // Check for errors and HTTP status code
-        if (!characterError && characterResponse.statusCode === 200) {
-          // Parse the JSON response for each character
-          const characterData = JSON.parse(characterBody);
-
-          // Get the character's name and print it
-          const characterName = characterData.name;
-          console.log(characterName);
-        } else {
-          console.log(`Failed to retrieve character data from URL: ${characterUrl}`);
-        }
-      });
+    const characterPromises = characters.map(async (characterUrl) => {
+      const characterResponse = await makeRequest(characterUrl);
+      const characterData = JSON.parse(characterResponse);
+      return characterData.name;
     });
-  } else {
-    console.log('Failed to retrieve film data.');
+
+    const characterNames = await Promise.all(characterPromises);
+
+    characterNames.forEach((characterName) => {
+      console.log(characterName);
+    });
+  } catch (error) {
+    console.error('Error:', error);
   }
-});
+}
+
+const movieid = process.argv[2];
+
+if (!movieid || isNaN(Number(movieid))) {
+  console.log('Usage: .js_file.js <film_number>');
+  process.exit(1);
+}
+
+getCharacters(movieid);
