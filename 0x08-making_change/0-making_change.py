a97b3dab27ecb343a8d919918f0a6ab66db08167
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-20 09:44:40 +0300

    Updated solution to task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,14 +5,21 @@
 def makeChange(coins, total):
     """A function that determines the fewest number of coins needed to
     meet a given amount"""
-    s = [0]
-    for i in range(1, total+1):
-        s.append(-1)
-        for coin_val in coins:
-            if i-coin_val >= 0 and \
-                    s[i-coin_val] != -1 and \
-                    (s[i] > s[i-coin_val] or
-                        s[i] == -1):
-                s[i] = 1 + s[i-coin_val]
+    if total < 0:
+        return -1
 
-    return s[total]
+    # Initialize a list to store the minimum number of
+    # coins needed for each amount from 0 to total.
+    # Set all values to float('inf') initially, except for s[0] which is 0.
+    s = [float('inf')] * (total + 1)
+    s[0] = 0
+
+    for coin_val in coins:
+        for i in range(coin_val, total + 1):
+            # Calculate the minimum number of coins needed
+            # for amount 'i' by considering the current coin.
+            s[i] = min(s[i], 1 + s[i - coin_val])
+
+    # If s[total] is still float('inf'), it means the total
+    # cannot be met by any combination of coins.
+    return -1 if s[total] == float('inf') else s[total]
