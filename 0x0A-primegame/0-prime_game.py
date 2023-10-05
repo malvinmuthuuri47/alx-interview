f25aef57850d2352d7c60f58825a9e1fbcdd7cbb
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-10-05 22:49:40 +0300

    Reverted changes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Prime Game"""
-from collections import defaultdict
 
 
 def isWinner(x, nums):
-    """Find winner"""
+    """A function that determines the winner of the prime game"""
     def is_prime(num):
-        """Check that number is prime"""
+        """A function that determines if the array has numbers greater
+        than 2 that fit to be prime numbers"""
         if num < 2:
             return False
         for i in range(2, int(num**0.5) + 1):
@@ -14,36 +14,25 @@ def isWinner(x, nums):
                 return False
         return True
 
-    def calculate_winner(n):
-        """Calculate winner function"""
-        m = defaultdict(int)
-        for i in range(2, n + 1):
-            if is_prime(i):
-                for j in range(i, n + 1, i):
-                    m[j] += 1
-
-        maria_turn = True
-        while True:
-            can_choose_prime = False
-            for num in m:
-                if m[num] > 0:
-                    can_choose_prime = True
-                    m[num] -= 1
-                    break
-
-            if not can_choose_prime:
-                return "Ben" if maria_turn else "Maria"
-
-            maria_turn = not maria_turn
-
+    # Create a list to store the winners for each round
     winners = []
-    for n in nums:
-        winner = calculate_winner(n)
-        winners.append(winner)
 
+    # Iterate through each round
+    for n in nums:
+        # Calculate the total count of prime numbers in the range [1, n]
+        prime_count = sum(1 for i in range(2, n + 1) if is_prime(i))
+
+        # Determine the winner based on the parity of the prime count
+        # If the count is even, Ben wins; otherwise, Maria wins
+        if prime_count % 2 == 0:
+            winners.append("Ben")
+        else:
+            winners.append("Maria")
+    # Count the number of times each player wins
     maria_wins = winners.count("Maria")
     ben_wins = winners.count("Ben")
 
+    # Determine the overall winner
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
